dc9b9d17830a4d14827c7b1ca30264e74a754894
kvv1618 <varun.kesharaju@gmail.com>
2021-12-17 00:12:33 +0530

    till cards

--- touchstone_assignment/lib/Screens/body.dart
+++ touchstone_assignment/lib/Screens/body.dart
@@ -16,6 +16,7 @@ class Body extends StatelessWidget {
               ),
               Container(
                 height: MediaQuery.of(context).size.height * 0.75,
+                width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -23,14 +24,52 @@ class Body extends StatelessWidget {
                     topRight: Radius.circular(30),
                   ),
                 ),
-                
-                // child: Column(
-                //   children: <Widget>[
-                //     Container(
-                //       child: Text("All rooms"),
-                //     ),
-                //   ],
-                // ),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.05,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Padding(padding: EdgeInsets.only(left: 20)),
+                        Container(
+                          child: Text(
+                            "All rooms",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: <Widget>[
+                        Card(
+                          child: Container(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.15,
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.3,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage(
+                                        "assets/bed.svg",
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
