23239e26ed54b468d488b82b9c7ccb87dbc1b326
kvv1618 <varun.kesharaju@gmail.com>
2021-12-17 11:58:07 +0530

    1st page

--- touchstone_assignment/lib/Screens/body.dart
+++ touchstone_assignment/lib/Screens/body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -13,64 +14,82 @@ class Body extends StatelessWidget {
             children: <Widget>[
               Container(
                 height: MediaQuery.of(context).size.height * 0.25,
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.75,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
-                child: Column(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.05,
-                    ),
-                    Row(
+                    Column(
                       children: <Widget>[
-                        Padding(padding: EdgeInsets.only(left: 20)),
-                        Container(
-                          child: Text(
-                            "All rooms",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
+                        Text(
+                          "Control \n Panel",
+                          style: TextStyle(
+                            color: Colors.white,
                           ),
                         ),
                       ],
                     ),
-                    Row(
+                    Column(
                       children: <Widget>[
-                        Card(
-                          child: Container(
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.15,
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.3,
-                                  decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                      image: AssetImage(
-                                        "assets/bed.svg",
-                                      ),
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+                        CircleAvatar(
+                          backgroundImage: AssetImage("assests/bed.svg"),
                         ),
                       ],
                     ),
                   ],
                 ),
               ),
+              // Container(
+              //   height: MediaQuery.of(context).size.height * 0.75,
+              //   width: MediaQuery.of(context).size.width,
+              //   decoration: BoxDecoration(
+              //     color: Colors.white,
+              //     borderRadius: BorderRadius.only(
+              //       topLeft: Radius.circular(30),
+              //       topRight: Radius.circular(30),
+              //     ),
+              //   ),
+              //   child: Column(
+              //     children: <Widget>[
+              //       SizedBox(
+              //         height: MediaQuery.of(context).size.height * 0.05,
+              //       ),
+              //       Row(
+              //         children: <Widget>[
+              //           Padding(padding: EdgeInsets.only(left: 20)),
+              //           Container(
+              //             child: Text(
+              //               "All rooms",
+              //               style: TextStyle(
+              //                 fontWeight: FontWeight.bold,
+              //               ),
+              //             ),
+              //           ),
+              //         ],
+              //       ),
+              //       Row(
+              //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              //         crossAxisAlignment: CrossAxisAlignment.center,
+              //         children: <Widget>[
+              //           Card(
+              //             child: Container(
+              //               height: MediaQuery.of(context).size.height * 0.15,
+              //               width: MediaQuery.of(context).size.width * 0.3,
+              //               child: Column(
+              //                 children: <Widget>[
+              //                   Row(
+              //                     children: [
+              //                       SvgPicture.asset("assests/bed.svg"),
+              //                     ],
+              //                   ),
+              //                 ],
+              //               ),
+              //             ),
+              //           ),
+              //         ],
+              //       ),
+              //     ],
+              //   ),
+              // ),
             ],
           ),
         ),
